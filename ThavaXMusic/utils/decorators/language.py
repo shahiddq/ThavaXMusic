fc50c72e8c7d8eb9e8ed39cced293729be636f97
Thava-OX <159293177+Thava-OX@users.noreply.github.com>
2024-07-30 07:49:44 +0530

    Update language.py

--- ThavaXMusic/utils/decorators/language.py
+++ ThavaXMusic/utils/decorators/language.py
@@ -1,6 +1,7 @@
 from ThavaXMusic.misc import SUDOERS
 from ThavaXMusic.utils.database import get_lang, is_maintenance
 from strings import get_string
+from config import SUPPORT_CHAT
 from ThavaXMusic import app
 
 
